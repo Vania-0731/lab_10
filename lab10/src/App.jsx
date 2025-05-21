3ed65ae43d001c3e392cd807e90bf4854ddb63d1
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-05-21 14:32:22 -0500

    Added CategoryPage and SeriePage to routes

--- lab10/src/App.jsx
+++ lab10/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import SeriePage from "./pages/SeriePage";
 
 function App() {
   return(
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage/>}/>
         <Route path="/home" element={<HomePage/>}/>
+        <Route path="/categories" element={<CategoryPage/>}/>
+        <Route path="/series" element={<SeriePage/>}/>
       </Routes>
     </BrowserRouter>
   )
